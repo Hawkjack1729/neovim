5146c293ced93880b0c8563687f42fe16eba7151
Hawkjack <hawkjack0721@gmail.com>
2024-04-12 16:09:37 +0530

    added snipet and code completion plugins

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require "hawkjack.options"
 require "hawkjack.keymaps"
 require "hawkjack.plugins"
 require "hawkjack.colorscheme"
+require "hawkjack.cmp_snipet"
+
--- lua/hawkjack/plugins.lua
+++ lua/hawkjack/plugins.lua
@@ -51,6 +51,19 @@ use "lunarvim/darkplus.nvim"
 use "sainnhe/sonokai"
 use "sainnhe/everforest"
 
+	-- cmp plugins
+	use "hrsh7th/nvim-cmp" -- The completion plugin
+	use "hrsh7th/cmp-buffer" -- buffer completions
+	use "hrsh7th/cmp-path" -- path completions
+	use "saadparwaiz1/cmp_luasnip" -- snippet completions
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-nvim-lua"
+
+	-- snippets
+    use 'mattn/emmet-vim'
+	use "L3MON4D3/LuaSnip" --snippet engine
+	use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
